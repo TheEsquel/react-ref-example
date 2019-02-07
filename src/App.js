60efcc7131d283eeb6d1ff45bae8f4027a6f1507
Rem Asadullin <rem.asadullin@gmail.com>
2019-02-07 09:48:48 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import Scroller from './Scroller';
+
+
+class Scroller extends React.Component {
+  constructor(props){
+    super(props);
+    this.refScroller = React.createRef();
+    this.state = {
+      isVisible : false
+    }
+    this.handleScroll = this.handleScroll.bind(this)
+  }
+
+  componentDidMount(){
+    console.log('did mount');
+    window.addEventListener('scroll', this.handleScroll)
+  }
+  handleScroll = () => {   
+    this.setState({
+      isVisible : true
+    })
+    console.log(this.state.isVisible);
+    
+    setTimeout(() => {
+      this.setState.isVisible = false
+    }, 1000)
+  }
+  componentWillUnmount(){
+  }
+  render(){
+      return(
+          <div ref = {scroller => this.refScroller = scroller}>
+           { this.state.isVisible  && "Scrolling"}   
+          </div>
+      )
+  }    
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+         <Scroller />
 
 
         </header>
